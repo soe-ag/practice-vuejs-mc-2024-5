eb911affd3e44bf1a3c5bf313c65d3fbc771619a
wind-ik <soeag.st@gmail.com>
2024-08-08 18:04:14 +0900

    add-shadcn-iconify

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,29 @@ import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VueRouter(), vue()],
+  plugins: [
+    VueRouter(),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (element) => element.startsWith('iconify-icon')
+        }
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
     }
   }
 })

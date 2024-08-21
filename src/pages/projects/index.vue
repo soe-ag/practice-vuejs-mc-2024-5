3f3ac085c6ff41e069f78930a0cc32ffea952f5e
wind-ik <soeag.st@gmail.com>
2024-08-22 00:02:54 +0900

    refactor-table-columns

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import type { ColumnDef } from '@tanstack/vue-table'
 import { RouterLink } from 'vue-router'
 import { projectsQuery, type Projects } from '@/utils/supaQueries'
+import { columns } from '@/utils/tableColumns/projectsColumns'
 
 usePageStore().pageData.title = 'Projects Page'
 
@@ -28,33 +28,6 @@ const getProjects = async () => {
 }
 
 await getProjects()
-
-const columns: ColumnDef<Projects[0]>[] = [
-  {
-    accessorKey: 'name',
-    header: () => h('div', { class: 'text-left' }, 'Name'),
-    cell: ({ row }) =>
-      h(
-        RouterLink,
-        {
-          to: `/projects/${row.original.slug}`,
-          class: 'text-left font-medium hover:bg-muted block w-full'
-        },
-        () => row.getValue('name')
-      )
-  },
-  {
-    accessorKey: 'status',
-    header: () => h('div', { class: 'text-left' }, 'Status'),
-    cell: ({ row }) => h('div', { class: 'text-left font-medium' }, row.getValue('status'))
-  },
-  {
-    accessorKey: 'collaborators',
-    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
-    cell: ({ row }) =>
-      h('div', { class: 'text-left font-medium' }, JSON.stringify(row.getValue('collaborators')))
-  }
-]
 </script>
 
 <template>
--- src/pages/tasks/index.vue
+++ src/pages/tasks/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router'
-import type { ColumnDef } from '@tanstack/vue-table'
 import { tasksWithProjectsQuery, type TasksWithProjects } from '@/utils/supaQueries'
+import { columns } from '@/utils/tableColumns/tasksColumns'
 
 usePageStore().pageData.title = 'Tasks Page'
 
@@ -18,53 +18,6 @@ const getTasks = async () => {
 }
 
 await getTasks()
-
-const columns: ColumnDef<TasksWithProjects[0]>[] = [
-  {
-    accessorKey: 'name',
-    header: () => h('div', { class: 'text-left' }, 'Name'),
-    cell: ({ row }) =>
-      h(
-        RouterLink,
-        {
-          to: `/tasks/${row.original.id}`,
-          class: 'text-left font-medium hover:bg-muted block w-full'
-        },
-        () => row.getValue('name')
-      )
-  },
-  {
-    accessorKey: 'status',
-    header: () => h('div', { class: 'text-left' }, 'Status'),
-    cell: ({ row }) => h('div', { class: 'text-left font-medium' }, row.getValue('status'))
-  },
-  {
-    accessorKey: 'due_date',
-    header: () => h('div', { class: 'text-left' }, 'Due Date'),
-    cell: ({ row }) => h('div', { class: 'text-left font-medium' }, row.getValue('due_date'))
-  },
-  {
-    accessorKey: 'projects',
-    header: () => h('div', { class: 'text-left' }, 'Project'),
-    cell: ({ row }) =>
-      row.original.projects
-        ? h(
-            RouterLink,
-            {
-              to: `/projects/${row.original.projects.slug}`,
-              class: 'text-left font-medium hover:bg-muted block w-full'
-            },
-            () => row.original.projects?.name
-          )
-        : ''
-  },
-  {
-    accessorKey: 'collaborators',
-    header: () => h('div', { class: 'text-left' }, 'Collaborators'),
-    cell: ({ row }) =>
-      h('div', { class: 'text-left font-medium' }, JSON.stringify(row.getValue('collaborators')))
-  }
-]
 </script>
 
 <template>

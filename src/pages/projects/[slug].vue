95aa9078d8acc71927779d968ea3f91e703638be
wind-ik <soeag.st@gmail.com>
2024-08-24 00:17:14 +0900

    make-project-page-dynamic

--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -5,6 +5,12 @@ const route = useRoute('/projects/[slug]')
 
 const project = ref<Project | null>(null)
 
+watch(
+  () => project.value?.name,
+  () => {
+    usePageStore().pageData.title = `Project: ${project.value?.name || 'No name'}`
+  }
+)
 const getProject = async () => {
   const { data, error } = await projectQuery(route.params.slug)
 
@@ -16,24 +22,20 @@ await getProject()
 </script>
 
 <template>
-  <Table>
+  <Table v-if="project">
     <TableRow>
       <TableHead> Name </TableHead>
-      <TableCell> Lorem ipsum dolor sit amet. </TableCell>
+      <TableCell> {{ project.name }}</TableCell>
     </TableRow>
     <TableRow>
       <TableHead> Description </TableHead>
       <TableCell>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad iure qui tempora ex nihil, ab
-        reprehenderit dolorem sunt veritatis perferendis? Repudiandae quis velit quasi ab natus quia
-        ratione voluptas deserunt labore sed distinctio nam fuga fugit vero voluptates placeat
-        aperiam, saepe excepturi eos harum consectetur doloremque perspiciatis nesciunt! Incidunt,
-        modi.
+        {{ project.description }}
       </TableCell>
     </TableRow>
     <TableRow>
       <TableHead> Status </TableHead>
-      <TableCell>In progress</TableCell>
+      <TableCell>{{ project.status }}</TableCell>
     </TableRow>
     <TableRow>
       <TableHead> Collaborators </TableHead>
@@ -41,8 +43,8 @@ await getProject()
         <div class="flex">
           <Avatar
             class="-mr-4 border border-primary hover:scale-110 transition-transform"
-            v-for="n in 5"
-            :key="n"
+            v-for="collab in project.collaborators"
+            :key="collab"
           >
             <RouterLink class="w-full h-full flex items-center justify-center" to="">
               <AvatarImage src="" alt="" />
@@ -54,7 +56,7 @@ await getProject()
     </TableRow>
   </Table>
 
-  <section class="mt-10 flex flex-col md:flex-row gap-5 justify-between grow">
+  <section v-if="project" class="mt-10 flex flex-col md:flex-row gap-5 justify-between grow">
     <div class="flex-1">
       <h2>Tasks</h2>
       <div class="table-container">
@@ -67,10 +69,10 @@ await getProject()
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow v-for="n in 5" :key="n">
-              <TableCell> Lorem ipsum dolor sit amet. </TableCell>
-              <TableCell> In progress </TableCell>
-              <TableCell> 22/08/2024 </TableCell>
+            <TableRow v-for="task in project.tasks" :key="task.id">
+              <TableCell> {{ task.name }} </TableCell>
+              <TableCell> {{ task.status }}</TableCell>
+              <TableCell> {{ task.due_date }}</TableCell>
             </TableRow>
           </TableBody>
         </Table>

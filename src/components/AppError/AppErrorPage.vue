624cf4e2fe3e0d8ab646cd84e4e0a43a2566dfd1
wind-ik <soeag.st@gmail.com>
2024-08-27 21:49:57 +0900

    error-handling

---
+++ src/components/AppError/AppErrorPage.vue
@@ -0,0 +1,63 @@
+<script setup lang="ts">
+const errorStore = useErrorStore()
+const error = ref(errorStore.activeError)
+const message = ref('')
+const customCode = ref(0)
+
+if (error.value) {
+  message.value = error.value.message
+  customCode.value = error.value.customCode ?? 0
+}
+
+const router = useRouter()
+
+router.afterEach(() => {
+  errorStore.activeError = null
+})
+</script>
+
+<template>
+  <section class="error">
+    <div>
+      <iconify-icon icon="lucide:triangle-alert" class="error__icon" />
+      <h1 class="error__code">{{ customCode }}</h1>
+      <p class="error__msg">{{ message }}</p>
+      <div class="error-footer">
+        <p class="error-footer__text">You'll find lots to explore on the home page.</p>
+        <RouterLink to="/">
+          <Button class="max-w-36"> Back to homepage </Button>
+        </RouterLink>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.error {
+  @apply mx-auto flex justify-center items-center flex-1 p-10 text-center -mt-20 min-h-[90vh];
+}
+
+.error__icon {
+  @apply text-7xl text-destructive;
+}
+
+.error__code {
+  @apply font-extrabold text-7xl text-primary;
+}
+
+.error__msg {
+  @apply text-3xl font-extrabold text-primary;
+}
+
+.error-footer {
+  @apply flex flex-col items-center justify-center gap-5 mt-6 font-light;
+}
+
+.error-footer__text {
+  @apply text-lg text-muted-foreground;
+}
+
+p {
+  @apply my-2;
+}
+</style>
---
+++ src/stores/error.ts
@@ -0,0 +1,15 @@
+import type { CustomError } from '@/types/Error'
+
+export const useErrorStore = defineStore('error', () => {
+  const activeError = ref<null | CustomError>(null)
+
+  const setError = ({ error, customCode }: { error: string; customCode: number }) => {
+    activeError.value = Error(error)
+
+    activeError.value.customCode = customCode
+  }
+  return {
+    activeError,
+    setError
+  }
+})

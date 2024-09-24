476d4dfb988a62dabd674af9d1ddc5d67e497eac
wind-ik <soeag.st@gmail.com>
2024-09-24 19:08:51 +0900

    register-user-with-supabase-auth-and-link-with-profile-table

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+
 const formData = ref({
   username: '',
   firstName: '',
@@ -7,6 +9,30 @@ const formData = ref({
   password: '',
   confirmPassword: ''
 })
+
+const router = useRouter()
+
+const signup = async () => {
+  const { data, error } = await supabase.auth.signUp({
+    email: formData.value.email,
+    password: formData.value.password
+  })
+
+  if (error) return console.log(error)
+  // console.log(data)
+
+  if (data.user) {
+    const { error } = await supabase.from('profiles').insert({
+      id: data.user.id,
+      username: formData.value.username,
+      full_name: formData.value.firstName + ' ' + formData.value.lastName
+    })
+
+    if (error) console.log('Profile Error: ', error)
+  }
+
+  router.push('/')
+}
 </script>
 
 <template>
@@ -23,7 +49,7 @@ const formData = ref({
           <Button variant="outline" class="w-full"> Register with Google </Button>
           <Separator label="Or" />
         </div>
-        <form class="grid gap-4">
+        <form class="grid gap-4" @submit.prevent="signup">
           <div class="grid gap-2">
             <Label id="username" class="text-left">Username</Label>
             <Input
